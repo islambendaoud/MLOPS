22cc4844948f7d504900b8a1764fff176ba897f0
Islam Bendaoud <islambendaoud@MacBook-Air-de-Islam.local>
2023-12-27 14:31:38 +0100

    suppression oublié

--- webapp/build.sh
+++
@@ -1,7 +0,0 @@
-
-docker build\
-    -t sentiment-analyzer:1.0-model_3_hyperopt-2\
-    --build-arg MLFLOW_SERVER_URI=http://host.docker.internal:5000\
-    --build-arg MODEL_NAME=model_3_hyperopt\
-    --build-arg MODEL_VERSION=2\
-    .
--- webapp/execute_docker.sh
+++
@@ -1,4 +0,0 @@
-docker run\
-    -p 8001:8000\
-    --name   sentiment-analyzer\
-    sentiment-analyzer:1.0-model_3_hyperopt-2
